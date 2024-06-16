0add2dd0fa4cc0d69f8a6c79bfb5fdf6928df8eb
Thu Rein <thureinminhtun347@gmail.com>
2024-06-16 15:59:25 +0630

    0.0.2.4v

--- assets/js/click-hover.js
+++ assets/js/click-hover.js
@@ -4,27 +4,33 @@ document.getElementById("menu-icon").addEventListener("click", function () {
 });
 // for animation appearance
 const skillSection = document.querySelector("#skills");
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    }
-  });
-}, {
-  threshold: 0.5
-});
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  },
+  {
+    threshold: 0.5,
+  }
+);
 
 observer.observe(skillSection);
 
 // top scroll button
 let mybutton = document.getElementById("myBtn");
 
-window.onscroll = function() {
+window.onscroll = function () {
   scrollFunction();
 };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
@@ -34,15 +40,16 @@ function scrollFunction() {
 function topFunction() {
   window.scrollTo({
     top: 0,
-    behavior: 'smooth'
+    behavior: "smooth",
   });
 }
 
-document.querySelectorAll('nav a'),forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
+document.querySelectorAll("nav a"),
+  foreach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth",
+      });
     });
-  })
-})
+  });
