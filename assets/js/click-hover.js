5aee4ce85934926f0c0beddea308c64cd326234e
Thu Rein <thureinminhtun347@gmail.com>
2024-06-16 12:38:44 +0630

    skill part commit

--- assets/js/click-hover.js
+++ assets/js/click-hover.js
@@ -2,3 +2,16 @@
 document.getElementById("menu-icon").addEventListener("click", function () {
   document.querySelector(".nav-list").classList.toggle("show");
 });
+// for animation appearance
+const skillSection = document.querySelector("#skills");
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+}, {
+  threshold: 0.5
+});
+
+observer.observe(skillSection);
